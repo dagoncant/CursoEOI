3892c0f5c2064d570a70c47370f4ae2e7a01019c
dagoncant <dagoncant@gmail.com>
2024-06-21 11:02:05 +0000

    Revert "Tercer cambio.. EEE"
    This reverts commit e747fe2e3e30c03fd12ca2164d829a88a8aa700f.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 import dice
 from time import sleep
 
@@ -6,5 +7,5 @@ def roll(amount:int, sides:int):
     return dice.roll(f'{amount}d{sides}')
 
 for idx, result in enumerate(roll(5,6)):
-    print(f'Lanzamiento EEE {idx+1} número obtenido {result}')
+    print(f'Lanzamiento {idx+1} número obtenido {result}')
     sleep(5)
