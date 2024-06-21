e747fe2e3e30c03fd12ca2164d829a88a8aa700f
dagoncant <dagoncant@gmail.com>
2024-06-21 10:55:26 +0000

    Tercer cambio.. EEE

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import dice
 from time import sleep
 
@@ -7,5 +6,5 @@ def roll(amount:int, sides:int):
     return dice.roll(f'{amount}d{sides}')
 
 for idx, result in enumerate(roll(5,6)):
-    print(f'Lanzamiento {idx+1} número obtenido {result}')
+    print(f'Lanzamiento EEE {idx+1} número obtenido {result}')
     sleep(5)
